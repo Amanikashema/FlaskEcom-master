19ad83b7e5102bed03178e24bc87f7d78004c34d
amani <amanikashema000@gmail.com>
2021-06-17 16:27:54 +0200

    updated info

--- market/tests/unit/forms_test.py
+++ market/tests/unit/forms_test.py
@@ -1,38 +1,44 @@
+from market.forms import RegisterForm
 from market.tests.unit.base_tests import BaseTest, db
 from flask import request
 from flask_login import current_user, AnonymousUserMixin, login_user, logout_user, login_required
 from market.models import User
 from flask import render_template, redirect, url_for, flash, request
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 
 
 class TestRegister(BaseTest):
     # Testing register form Url
     def test_register_form_url(self):
         with self.app:
-            response = self.app.get('/register', data=dict(email_address='email@gmail.com', username='amani', password1='pass1234',
-                                               password2='pass1234'),follow_redirects=True)
-            self.assertEqual(response.status_code,200)
-            self.assertIn(request.url,'http://localhost/register')
-
+            response = self.app.get('/register',
+                                    data=dict(email_address='email@gmail.com', username='amani', password1='pass1234',
+                                              password2='pass1234'), follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(request.url, 'http://localhost/register')
 
     def test_valid_username(self):
         with self.app:
-            response = self.app.post('/register',  data=dict(email_address='email@gmail.com', username='amani', password1='pass1234',
-                                               password2='pass1234'), follow_redirects=True)
+            self.app.post('/register',
+                          data=dict(email_address='email@gmail.com', username='amani', password1='pass1234',
+                                    password2='pass1234'), follow_redirects=True)
 
-            user = db.session.query(User).filter_by(username='amani').first()
+        # Create an object because the form is expecting one from the post
+        class email():
+            data = 'test6'
 
-            # chek if user is in database
-            self.assertTrue(user)
-            validation_error = 'Username already exists! Please try a different username'
-            # Test validation if username already exists
-            self.assertTrue(validation_error,'Username already exists! Please try a different username')
+        # use raise exception as context manager because it will store the caught exception in its exception attribute
+        with self.assertRaises(ValidationError) as context:
+            # call validate username method
+            RegisterForm().validate_username(email)
+            self.assertEqual('Email Address already exists! Please try a different email address',
+                             str(context.exception))
 
     def test_validate_email(self):
         with self.app:
             self.app.post('/register',
-                                    data=dict(email_address='email@gmail.com', username='amani', password1='pass1234',
-                                              password2='pass1234'), follow_redirects=True)
+                          data=dict(email_address='email@gmail.com', username='amani', password1='pass1234',
+                                    password2='pass1234'), follow_redirects=True)
 
             user = db.session.query(User).filter_by(email_address='email@gmail.com').first()
 
@@ -46,15 +52,8 @@ class TestRegister(BaseTest):
     def test_user_registered(self):
         with self.app:
             response = self.app.post('/register',
-                                    data=dict(email_address='email@gmail.com', username='amani', password1='pass1234',
-                                              password2='pass1234'), follow_redirects=True)
-            self.assertEqual(response.status_code,200)
+                                     data=dict(email_address='email@gmail.com', username='amani', password1='pass1234',
+                                               password2='pass1234'), follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
             # test that user is created
             self.assertIn(b'Account created successfully! You are now logged in as amani', response.data)
-
-
-
-
-
-
-
