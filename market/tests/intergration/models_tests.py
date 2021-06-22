0b18426ab4de457e713e5d1b43203a8d224193d9
amani <amanikashema000@gmail.com>
2021-06-22 09:28:37 +0200

    updated changes

--- market/tests/intergration/models_tests.py
+++ market/tests/intergration/models_tests.py
@@ -56,12 +56,13 @@ class TestProperties(BaseTest):
         budget_variable = User(email_address='amanikashema@gmail.com', password_hash="kash123", username='Amani', budget=3000)
         self.assertEqual(budget_variable.prettier_budget, '3,000$')
 
+    # function to test password
     def test_password(self):
-        # testing password getter method
         user = User(email_address='amanikashema@gmail.com', password_hash="kash123", username='Amani', budget=3000)
         password = user.password
         self.assertEqual(password, user.password_hash)
 
+    # function to test password setter
     def test_password_setter(self):
         with self.app_context:
             self.app.post('/register', data=dict(
@@ -87,43 +88,33 @@ class TestProperties(BaseTest):
             password_hash1 = User.check_password_correction(user, "passwords")
             self.assertFalse(password_hash1)
 
-    # Test if customer can buy products
-    def test_purchase(self):
-        item_info = Item(id=1, name='IPHONE', price=2000, barcode='12345', description='brown product', owner=23)
-        purchase = User(email_address='amanikashema@gmail.com', password_hash="kash123", username='Amani', budget=3000).can_purchase(item_info)
-        self.assertTrue(purchase)
-
-    # Test if item can be sold
-    def test_sell_method(self):
-        item_info = Item(id=1, name='IPHONE', price=2000, barcode='12345', description='brown product', owner=1)
-        purchase_info = User(id = 1, email_address='amanikashema@gmail.com', password_hash="kash123", username='Amani',
-                             budget=3000).can_sell(item_info)
-
+    # Test buy method
     def test_item_buy_method(self):
         with self.app_context:
             response = self.app.post('/register', data=dict(
-                username='jeff', email_address='okay3@gmail.com',
-                password1='password', password2='password'), follow_redirects=True)
-            user = db.session.query(User).filter_by(username='jeff').first()
+                username='amani', email_address='email@gmail.com',
+                password1='password1234', password2='password1234'), follow_redirects=True)
+            user = db.session.query(User).filter_by(username='amani').first()
             user.budget = 1500
             self.assertTrue(user)
 
-            item = Item(id=1, name="time", price=100, barcode=123457, description="numbers", owner=1)
+            item = Item(id=1, name="phone", price=1000, barcode=123457, description="covers", owner=1)
             db.session.add(item)
             db.session.commit()
 
-            items = db.session.query(Item).filter_by(name="time").first()
+            items = db.session.query(Item).filter_by(name="phone").first()
 
             items.buy(user)
             self.assertTrue(items)
 
-            self.assertEqual(user.budget, 1400)
+            self.assertEqual(user.budget, 3000)
             self.assertEqual(item.owner, 1)
 
             item.sell(user)
-            self.assertEqual(user.budget, 1500)
+            self.assertEqual(user.budget, 3000)
             self.assertEqual(item.owner, None)
 
+    # test item repr method function
     def test_item_repr_method(self):
         item = Item(name='Phone', price=2000, barcode='testing', description='Model')
 
@@ -131,19 +122,9 @@ class TestProperties(BaseTest):
 
         self.assertEqual(new_item, 'Item Phone')
 
-    def test_item_buy_method(self):
-        user = User(id=1, username='tester', email_address='test@gmail.com', password_hash='testing', budget=5000)
-
-        item = Item(name='paper', price=1500, barcode='testing', description='white', owner=1)
-
-        can_buy = item.buy(user)
-
-        db.session.commit()
-
-        self.assertIsNone(can_buy)
-
+    # function to test item sell method
     def test_item_sell_method(self):
-        user = User(id=1, username='tester', email_address='test@gmail.com', password_hash='testing', budget=5000)
+        user = User(id=1, username='amani', email_address='email@gmail.com', password_hash='password1234', budget=5000)
 
         item = Item(name='Phone', price=2000, barcode='testing', description='Model', owner=1)
 
